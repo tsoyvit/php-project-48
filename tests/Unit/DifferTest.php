445fef5ef06f28625314c04d54f118964b36959f
Vitaliy <tsoyvetal@gmail.com>
2025-03-29 20:45:28 +0300

    commit 9.9

--- tests/Unit/DifferTest.php
+++ tests/Unit/DifferTest.php
@@ -5,7 +5,9 @@ namespace Tests\Unit;
 use Exception;
 use PHPUnit\Framework\TestCase;
 
-use function Differ\Differ\{buildDiff, genDiff};
+use function Differ\Differ\genDiff;
+use function Differ\Formatters\Plain\stringifyPlainValue;
+use function Differ\Formatters\Stylish\stringifyValue;
 
 class DifferTest extends TestCase
 {
@@ -14,32 +16,42 @@ class DifferTest extends TestCase
      */
     public function testGenDiffJsonYamlNested()
     {
+        // Фикстуры
         $jsonPath1 = realpath(__DIR__ . '/../Fixtures/fileNested1.json');
         $jsonPath2 = realpath(__DIR__ . '/../Fixtures/fileNested2.json');
         $yamlPath1 = realpath(__DIR__ . '/../Fixtures/fileNested1.yaml');
         $yamlPath2 = realpath(__DIR__ . '/../Fixtures/fileNested2.yaml');
+        // формат вывода stylish
         $expectedPathStylish = realpath(__DIR__ . '/../Fixtures/Formatters/expectedNestedStylish.txt');
         $expectedStylish = file_get_contents($expectedPathStylish);
-
         $this->assertEquals($expectedStylish, genDiff($jsonPath1, $jsonPath2));
         $this->assertEquals($expectedStylish, genDiff($yamlPath1, $yamlPath2));
-
-        $expectedPathPlain = realpath(__DIR__ . '/../Fixtures/Formatters/expectedNestedPlain.txt');
-        $expectedPlain = file_get_contents($expectedPathPlain);
-        $this->assertEquals($expectedPlain, genDiff($jsonPath1, $jsonPath2, 'plain'));
+        // формат вывода plain
+        $expectedPathStylish = realpath(__DIR__ . '/../Fixtures/Formatters/expectedNestedPlain.txt');
+        $expectedStylish = file_get_contents($expectedPathStylish);
+        $this->assertEquals($expectedStylish, genDiff($jsonPath1, $jsonPath2, 'plain'));
+        $this->assertEquals($expectedStylish, genDiff($yamlPath1, $yamlPath2, 'plain'));
+        // формат вывода json
+        $expectedPathStylish = realpath(__DIR__ . '/../Fixtures/Formatters/expectedNestedJson.txt');
+        $expectedStylish = file_get_contents($expectedPathStylish);
+        $this->assertEquals($expectedStylish, genDiff($jsonPath1, $jsonPath2, 'json'));
+        $this->assertEquals($expectedStylish, genDiff($yamlPath1, $yamlPath2, 'json'));
     }
 
-    public function testBuildDiff()
+    public function testStringifyValuePlain()
     {
-        $file1Data = (object)['a' => 1, 'b' => 2];
-        $file2Data = (object)['a' => 1, 'b' => 2, 'c' => 3];
+        $this->assertEquals('true', stringifyPlainValue(true));
+        $this->assertEquals('false', stringifyPlainValue(false));
+        $this->assertEquals('null', stringifyPlainValue(null));
+        $this->assertEquals('123', stringifyPlainValue(123));
+    }
 
-        $expectedDiff = [
-            ['type' => 'unchanged', 'key' => 'a', 'value' => 1],
-            ['type' => 'unchanged', 'key' => 'b', 'value' => 2],
-            ['type' => 'added', 'key' => 'c', 'value' => 3],
-        ];
-
-        $this->assertEquals($expectedDiff, buildDiff($file1Data, $file2Data));
+    public function testStringifyValue()
+    {
+        $this->assertEquals('true', stringifyValue(true));
+        $this->assertEquals('false', stringifyValue(false));
+        $this->assertEquals('null', stringifyValue(null));
+        $this->assertEquals('123', stringifyValue(123));
+        $this->assertEquals('hello', stringifyValue('hello'));
     }
 }
